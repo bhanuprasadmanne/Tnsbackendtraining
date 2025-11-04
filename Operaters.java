2b946ff1f03d186406b44f249a45f1782f659ab9
bhanuprasadmanne <bhanuprasadoff@gmail.com>
2025-11-05 00:00:00 +0530

    Add files via upload

---
+++ Operaters.java
@@ -0,0 +1,28 @@
+public class Operaters{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Arithmetic Operators
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (b - a));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (b / a));
+        System.out.println("Modulus: " + (b % a));
+
+        // Relational Operators
+        System.out.println("Is a equal to b? " + (a == b));
+        System.out.println("Is a not equal to b? " + (a != b));
+        System.out.println("Is a greater than b? " + (a > b));
+        System.out.println("Is a less than b? " + (a < b));
+        System.out.println("Is a greater than or equal to b? " + (a >= b));
+        System.out.println("Is a less than or equal to b? " + (a <= b));
+
+        // Logical Operators
+        boolean x = true;
+        boolean y = false;
+        System.out.println("Logical AND: " + (x && y));
+        System.out.println("Logical OR: " + (x || y));
+        System.out.println("Logical NOT: " + (!x));
+    }
+}
