e7aef15e50ef6301bcca1acc39f0a7b47fd44b1e
bhanuprasadmanne <bhanuprasadoff@gmail.com>
2025-11-05 00:11:08 +0530

    Add files via upload

---
+++ WhileLoopExample.java
@@ -0,0 +1,11 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int count = 1;
+
+        // Using a while loop to print numbers from 1 to 5
+        while (count <= 5) {
+            System.out.println("Count: " + count);
+            count++;
+        }
+    }
+}
